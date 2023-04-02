87c6d57187b31c7491d41477cd32d9c54a34fd56
S-P-H-E <sphe.g.personal@gmail.com>
2023-04-02 20:35:23 +0200

    UI Change

--- components/Quote.js
+++ components/Quote.js
@@ -1,6 +1,6 @@
 export default function Quote({ Quote, Author }) {
     return (
-      <div className="bg-[#161616] text-white font-medium p-5 rounded-xl flex flex-col justify-center items-center w-full max-w-[400px]">
+      <div className="bg-[#161616] text-white font-medium p-5 rounded-xl flex flex-col justify-center items-center w-full max-w-[400px] md:max-w-[700px] md:h-[250px] md:text-xl transition-all hover:scale-105 cursor-pointer">
         <p className="whitespace-pre-wrap text-center">{Quote}</p>
         <h1 className="my-2 text-[#7E7C7D]">{Author}</h1>
       </div>
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,16 @@
-import Nav from '@/components/Nav'
 import Quote from '@/components/Quote'
 import Head from 'next/head'
 import quotes from '@/public/quotes.json'
 import { useState } from 'react';
+import Link from 'next/link'
+import { MdEmail } from "react-icons/md"
+import { BsTwitter } from "react-icons/bs"
 
 import { AiOutlineSearch } from "react-icons/ai"
+import { FiHome } from "react-icons/fi"
 
 export default function Home() {
-  const [quotesPerPage, setQuotesPerPage] = useState(3)
+  const [quotesPerPage, setQuotesPerPage] = useState(1)
   const [searchValue, setSearchValue] = useState('');
   const filteredQuotes = quotes.filter(quote => (
     quote.quote.toLowerCase().includes(searchValue.toLowerCase())
@@ -19,7 +22,7 @@ export default function Home() {
   const currentQuotes = filteredQuotes.slice(firstQuote, lastQuote)
 
   const loadMoreQuotes = () => {
-    setQuotesPerPage(quotesPerPage + 3)
+    setQuotesPerPage(quotesPerPage + 2)
   }
 
   return (
@@ -31,18 +34,26 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Nav />
-      <div className='p-5 flex flex-col gap-4 justify-center md:justify-start items-center md:items-start'>
-        <div className='flex justify-center items-center border border-[#58585A] rounded-full w-full max-w-[400px]'>
-          <AiOutlineSearch className='text-[#58585A] m-2'/>
-          <input
-            className='bg-transparent w-full outline-none placeholder:text-[#58585A] caret-white text-white'
-            placeholder='Search Quotes'
-            value={searchValue}
-            onChange={event => setSearchValue(event.target.value)}
-          />
+      <div className='m-5 flex flex-col gap-4 justify-center items-center'>
+        {/* Navbar */}
+        <div className="p-7 m-5 flex justify-between items-center text-white flex-col md:flex-row bg-[#161616] rounded-2xl w-full md:max-w-[700px]">
+            <div className="flex flex-col gap-5 w-full">
+              <div className='flex justify-between items-center'>
+                <h1 className='text-2xl'>Quotify</h1>
+              </div>
+                <div className='flex justify-center items-center border border-[#58585A] rounded-full w-full'>
+                  <AiOutlineSearch className='text-[#58585A] m-2'/>
+                  <input
+                    className='bg-transparent w-full outline-none placeholder:text-[#58585A] caret-white text-white'
+                    placeholder='Search Quotes'
+                    value={searchValue}
+                    onChange={event => setSearchValue(event.target.value)}
+                  />
+                </div>
+            </div>
         </div>
-          <div className='gap-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 grid-flow-row'>
+
+          <div className='flex flex-col gap-4'>
             {currentQuotes.length > 0 ? (
               currentQuotes.map((quote, index) => (
                   <Quote key={index} Quote={quote.quote} Author={quote.author} />
@@ -55,7 +66,7 @@ export default function Home() {
           {filteredQuotes.length > quotesPerPage ? (
             <button className="bg-white text-gray-800 py-2 px-4 rounded-lg mt-5" onClick={loadMoreQuotes}>Load more quotes</button>
           ) : null}
-        </div>
+      </div>
     </>
   )
 }
